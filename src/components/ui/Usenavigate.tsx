a64e0f5189c29a370161baa12a3d15b07d0ea3b6
Guna Vardhan <varivardhan15@gmail.com>
2025-09-12 15:23:16 +0530

    ui:change button color

--- src/components/ui/Usenavigate.tsx
+++ src/components/ui/Usenavigate.tsx
@@ -11,7 +11,7 @@ export default function MyButton({href, label}:MyButtonProps){
 
     return(
         <div>
-            <button onClick={()=> navigate(href)} className="px-4 py-2 bg-blue-500 text-white rounded-lg"  >
+            <button onClick={()=> navigate(href)} className="px-4 py-2 bg-gray-700 hover:bg-gray-500 text-white rounded-lg"  >
                 {label}
             </button>
         </div>
