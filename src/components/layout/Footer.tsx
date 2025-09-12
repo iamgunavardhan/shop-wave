f68059ae6b31fa8e1b1d407090be2bbb13a9605f
Guna Vardhan <varivardhan15@gmail.com>
2025-09-12 15:50:37 +0530

    feat:create footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import Container from "../ui/Container";
+
+const FooterLins =[
+    {href:'/',lable:'Home'},
+    {href:'/about',lable:'About'},
+    {href:'/products',lable:'Products'},
+   ]
+
+   export default function(){
+    return(
+        <footer></footer>
+    )
+   }
