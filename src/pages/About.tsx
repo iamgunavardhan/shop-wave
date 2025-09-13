f66db4e99cd2f084365437eb923d9f0441fe7523
Guna Vardhan <varivardhan15@gmail.com>
2025-09-13 16:20:25 +0530

    ui:update spacing between h2 and p

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 export default function About(){
     return(
-        <div className="p-10 text-center h-full flex items-center justify-center">
+        <div className="p-10 flex-col h-full flex items-center justify-center space-y-4">
             <h2 className="text-2xl font-bold text-gray-900 ">About us</h2>
             <p className="text-lg text-gray-700 font-medium"> ShopWave is your one-stop shop for amazing products.</p>
         </div>
