4949145d2484e6044fa5050ff635549702f13b28
Guna Vardhan <varivardhan15@gmail.com>
2025-09-13 15:31:01 +0530

    fix:code update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@
 export default function Home(){
     return(
       
-         <div className="flex justify-between items-center h-full ">
-             <h1 className="text-2xl font-bold text-gray-900">Welcome to the SHOPwave.</h1>
+         <div className="flex items-center justify-center  h-full p-8 ">
+             <h1 className="text-2xl font-bold text-gray-900 ">Welcome to the SHOPwave.</h1>
         </div>
        
     )
