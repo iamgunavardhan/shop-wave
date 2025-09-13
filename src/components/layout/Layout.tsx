135c6b454eda064dac504739c9bad2582605f631
Guna Vardhan <varivardhan15@gmail.com>
2025-09-13 15:01:53 +0530

    fix:code change

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import type{ ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Container from "../ui/Container";
 
 type LayoutProps ={
     children: ReactNode
@@ -10,7 +11,9 @@ export default function Layout({children}:LayoutProps){
     return(
         <div className="flex flex-col min-h-screen">
             <Navbar/>
-            <main className="flex-1">{children}</main>
+            <main className="flex-1">
+                <Container>{children}</Container>
+            </main>
             <Footer/>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import Container from "../components/ui/Container";
 
 export default function Home(){
     return(
-       <Container>
-         <div className="flex justify-between items-center">
+      
+         <div className="flex justify-between items-center h-96">
              <h1 className="text-2xl font-bold text-gray-900">Welcome to the SHOPwave.</h1>
         </div>
-       </Container>
+       
     )
 }
