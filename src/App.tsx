92c73c382ab9fd26f2c9df39beeb84336988c395
Guna Vardhan <varivardhan15@gmail.com>
2025-09-17 15:29:16 +0530

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import ProductDetails from "./pages/Productdetail";
 
 
  function App(){
@@ -13,6 +14,7 @@ import Products from "./pages/Products";
                     <Route path="/" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
                     <Route path="/products" element={<Products/>}/>
+                    <Route path="/products/:id" element={<ProductDetails/>}/>
                 </Routes>
             </Layout>
             
