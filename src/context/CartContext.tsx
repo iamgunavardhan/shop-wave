5d294c4bfe1a88b5dd13833319f67aa444d6f1dd
Guna Vardhan <varivardhan15@gmail.com>
2025-09-18 15:51:29 +0530

    update cartcontext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -13,6 +13,8 @@ interface CartContextType{
     cartItems: CartItem[]
     addToCart: (item: Omit<CartItem, "quantity">) => void
     removeFromCart:(id: number) => void
+    increaseQty: (id: number) => void;
+    decreaseQty: (id: number) => void;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined)
@@ -36,8 +38,25 @@ export  function CartProvider({children} : {children: ReactNode}) {
     setCartItems((prev)=> prev.filter((item)=> item.id !== id))
   }
 
+  const increaseQty = (id:number) => {
+    setCartItems((prev) => 
+    prev.map((item) =>
+    item.id === id? {...item,quantity:item.quantity + 1} : item
+    )
+    )
+  }
+
+  const decreaseQty = (id:number) => {
+    setCartItems((prev) => 
+    prev.map((item) =>
+    item.id === id? {...item,quantity:item.quantity - 1} : item
+    )
+    .filter((item) => item.quantity > 0)
+    )
+  }
+
   return(
-    <CartContext.Provider value={{cartItems,addToCart, removeFromCart}}>
+    <CartContext.Provider value={{cartItems,addToCart, removeFromCart, increaseQty, decreaseQty}}>
       {children}
     </CartContext.Provider>
   )
