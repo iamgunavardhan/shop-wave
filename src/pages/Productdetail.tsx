a899d3a340d24a8170ded1e172fa76ff33e238ea
Guna Vardhan <varivardhan15@gmail.com>
2025-09-18 14:09:42 +0530

    update product details

--- src/pages/Productdetail.tsx
+++ src/pages/Productdetail.tsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import MockProducts from "../components/products/Mockproducts";
+import useCartContext from "../context/Usecontext";
 
 
 export default function ProductDetails(){
     const{ id } = useParams<{id: string}>()
+    const {addToCart} = useCartContext()
     const product = MockProducts.find((p)=>p.id === Number(id))
 
     if(!product){
@@ -20,7 +22,14 @@ export default function ProductDetails(){
             <div >
                 <h2 className="mt-4 font-semibold text-gray-800 text-lg">{product.name}</h2>
                 <p className="text-md font-normal text-gray-600 mt-6 ">${product.price.toFixed(2)}</p>
-                <button className="mt-6 rounded-lg bg-indigo-500 hover:bg-indigo-400 transition-all w-full py-2">Add Cart</button>
+                <button
+                onClick={()=> addToCart({
+                    id:product.id,
+                    name:product.name,
+                    price:product.price,
+                    image:product.image,
+                })}
+                className="mt-6 rounded-lg bg-indigo-500 hover:bg-indigo-400 transition-all w-full py-2">Add Cart</button>
             </div>
         </div>
     )
