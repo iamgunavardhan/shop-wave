41fbc1548e3cbde00560737d3d0ad0363e8ac5e6
Guna Vardhan <varivardhan15@gmail.com>
2025-09-22 16:05:14 +0530

    update login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,7 +32,7 @@ export default function Login(){
                 required
                 className="w-full border px-4 py-2 rounded-lg" />
             <button  type="submit"
-          className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700"></button>
+          className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700">Login</button>
         </form>
     </div>
   )
