fce8188c6bb669d176c89cbef9287c888cad56f0
Guna Vardhan <varivardhan15@gmail.com>
2025-09-23 14:49:53 +0530

    update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,8 +26,31 @@ export default function Home(){
              </section>
 
              {/* Featured Section */}
-             <section>
-                
+             <section  className="max-w-7xl mx-auto px-6 lg:px-12 py-16">
+                 <h2  className="text-2xl lg:text-3xl font-bold text-gray-800 text-center mb-10"> Featured Categories</h2>
+                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8" >
+                    <Link to="/products" className="group relative overflow-hidden rounded-xl shadow-lg">
+                      <img src="/Fashion.png" alt="Fashion" 
+                      className="w-full h-64 object-cover group-hover:scale-110 transition-transform"/>
+                      <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+                         <span className="text-white text-xl font-semibold"> Fashion</span>
+                      </div>
+                    </Link>
+
+                    <Link to="/products" className="group relative overflow-hidden rounded-xl shadow-lg">
+                       <img src="/Accessories.png" alt="Accessories" className="w-full h-64 object-cover group-hover:scale-110 transition-transform"/>
+                        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+                           <span className="text-white text-xl font-semibold">Accessories</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/products" className="group relative overflow-hidden rounded-xl shadow-lg">
+                        <img src="/Electronics.png" alt="Electronics" className="w-full h-64 object-cover group-hover:scale-110 transition-transform" />
+                          <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+                            <span className="text-white text-xl font-semibold">Electronics</span>
+                          </div>
+                    </Link>
+                 </div>
              </section>
             
         </div>
